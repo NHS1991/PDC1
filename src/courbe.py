e4127484ba50e81e4c635dd715d5976100808164
NHS1991 <hoangson1991@gmail.com>
2015-10-18 14:37:33 +0200

    New inverted index func + verify zipf's law, tf-idf

--- src/courbe.py
+++ src/courbe.py
@@ -6,70 +6,73 @@ import matplotlib.pyplot as pyplot
 import math
 from os import listdir
 from os.path import isfile, join, splitext
-import operator
-import numpy as np
 
 def getKey(item):
     return item[2]
 
 if __name__ == '__main__':
-    if len(sys.argv)==6:
+    if len(sys.argv)==5:
         f_read = open(sys.argv[1],"r")
         f_write_freq = open(sys.argv[2],"w")
         f_write_tfidf = open(sys.argv[3],"w")
-        doc_tfidf = sys.argv[4]
-        s = float(sys.argv[5])
-        list_fr = []
-        list_fr_sort = []
+        doc_id_tfidf = sys.argv[4]
+        list_fr = []  # liste de fréquence trié par le nombre d'occurences
+        list_fr_sort = [] #liste des termes avec leur fréquence et leur nombre de documents d'apparition, trié par le nombre d'occurences de terme
         list_tfidf_sort = []
         docs_path = "../../latimes/"
         files = [f for f in listdir(docs_path) if isfile(join(docs_path,f)) and splitext(f)[1]=='' and 'la' in splitext(f)[0]]
-        nb_files = len(files)
-        for line in f_read:
-            line_arr = line.split("||")
-            nb_doc = len(line_arr) - 2
-            freq = int(line_arr[1])
-            term = line_arr[0]
-            i =2
-            freq_in_doctfidf = 0
-            while i<len(line_arr):
-                line_arr_arr = line_arr[i].split("|")
-                if line_arr_arr[0] == doc_tfidf:
-                    freq_in_doctfidf = int(line_arr_arr[1])
-                    break
-                i +=1
-            idf_word = math.log10(nb_files/nb_doc)
-            tfidf_word = freq_in_doctfidf* idf_word
+        nb_DOCs = 0 #nombre de l'élément DOC dans tous les fichiers
+        for file in files:
+            file_read = open(docs_path+file,"r")
+            file_data = file_read.read()
+            file_read.close()
+            nb_DOCs += file_data.count("<DOC>")
+        nb_terms = 0
+        for term_line in f_read:
+            nb_terms += 1
+            term_arr = term_line.split("||")
+            term = term_arr[0].split("|")[0] # le terme
+            nb_doc = int(term_arr[0].split("|")[1]) #le nombre de document contient ce terme
+            freq = 0 #le nombre d'occurences de ce terme dans tous les fichiers
+            i = 1
+            freq_in_doctfidf=0
+            while i<len(term_arr):
+                doc_arr = term_arr[i].split("|")
+                if doc_arr[0] == doc_id_tfidf:
+                    freq_in_doctfidf = int(doc_arr[1])
+                freq += int(doc_arr[1])
+                i += 1
+            idf_term = math.log10(nb_DOCs/nb_doc)
+            tfidf_term = freq_in_doctfidf * idf_term
             list_fr_sort.append([term,nb_doc,freq])
-            list_tfidf_sort.append([term,nb_doc,tfidf_word,freq_in_doctfidf,freq])
+            list_tfidf_sort.append([term,nb_doc,tfidf_term,freq_in_doctfidf,freq])
         list_fr_sort = sorted(list_fr_sort,key=getKey,reverse=True)
         list_tfidf_sort = sorted(list_tfidf_sort,key=getKey,reverse=True)
         pos = 0
         list_fr_zipf = []
         sum = 0
-        for i in range(1,len(list_fr_sort)):
-            sum += 1/math.pow(i,s)
         for word in list_fr_sort:
             list_fr.append(word[2])
             f_write_freq.write(word[0]+"|"+str(word[1])+"|"+str(word[2])+"\n")
             pos +=1
-            list_fr_zipf.append((1/pow(pos,s))/sum)
+            list_fr_zipf.append((list_fr_sort[0][2])/pos)
         f_write_freq.close()
         list_tfidf = []
         for word in list_tfidf_sort:
             f_write_tfidf.write(word[0]+"|"+str(word[1])+"|"+str(word[4])+"|"+str(word[3])+"|"+str(word[2])+"\n")
             list_tfidf.append(word[2])
         f_write_tfidf.close()
+        x = [math.log(i) for i in range(1,nb_terms+1)]
         pyplot.figure(1)
         pyplot.subplot(3,1,1)
         pyplot.subplots_adjust(hspace = 1)
-        pyplot.plot(list_fr)#list_rang,list_fr)
+        pyplot.plot(x,list_fr)#list_rang,list_fr)
         pyplot.title("Courbe de la frequence en fonction du rang")
         pyplot.subplot(3,1,2)
-        pyplot.plot(list_fr_zipf)
+        pyplot.plot(x,list_fr_zipf)
         pyplot.title("Loi de Zipf")
         pyplot.subplot(3,1,3)
-        pyplot.plot(list_tfidf)
+        pyplot.plot(x,list_tfidf)
         pyplot.title("Term frequency-Inverse document frequency")
         pyplot.savefig("frequency.png")
         f_read.close()
--- src/indexInverse.py
+++ src/indexInverse.py
@@ -4,10 +4,9 @@ __author__ = 'NHS'
 
 import sys
 import re
-import matplotlib.pyplot as pyplot
-from os import listdir
-from os.path import isfile, join, splitext
-from collections import defaultdict
+from os import listdir, makedirs
+from os.path import isfile, join, splitext, exists
+from collections import defaultdict,OrderedDict
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
@@ -15,8 +14,7 @@ porter_stemmer=PorterStemmer()
 
 class IndexInverse:
     def __init__(self):
-        self.index = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))    #index inversé
-
+        self.index = defaultdict(lambda: defaultdict())#index inversé
 
     def getStopwords(self):
         stop_words=stopwords.words('english')
@@ -27,7 +25,7 @@ class IndexInverse:
         doc=doc.lower()
         doc=re.sub(r"[^a-z0-9 ]",' ',doc) #remplacer les caractères non-alphabetiques par espace
         doc=[x for x in doc.split() if x not in self.stopwords]  #eliminer les mots vides
-        doc=[porter_stemmer.stem(word) for word in doc]
+        doc=[porter_stemmer.stem(word) for word in doc] #Récuperer les termes
         return doc
 
 
@@ -58,93 +56,116 @@ class IndexInverse:
         return doc_dict
 
 
-    def writeFileIndex(self):
-        f=open(self.indexFile, 'w')
-        for term in self.index.iterkeys():
+    def writeFileIndex(self,index_file):
+        file_write=open(self.indexFolder+index_file, 'w')
+        inverse_index = sorted(self.index.items(), key=lambda item: item[0])
+        self.index.clear()
+        for elem in inverse_index:
+            term = elem[0]
             postinglist=[]
-            count_all = 0
-            position_dict = self.index[term]
-            for file_name in position_dict.keys():
-                list_pos_by_doc_id = []
-                count_in_doc = 0
-                for doc_id in position_dict[file_name].keys():
-                    positions = position_dict[file_name][doc_id]
-                    len_pos_list = len(positions)
-                    count_in_doc +=len_pos_list
-                    list_pos_by_doc_id.append(str(doc_id)+':'+','.join(str(pos) for pos in positions))
-                postinglist.append(file_name+'|'+str(count_in_doc)+"|"+";".join(list_pos_by_doc_id))
-                count_all += count_in_doc
-            print >> f, term+'||'+str(count_all)+'||'+'||'.join(postinglist)
-        f.close()
+            docs_list = sorted(elem[1].items(),key=lambda item:item[0])
+            for doc in docs_list:
+                doc_id = doc[0]
+                nb_occur = doc[1]
+                postinglist.append(str(doc_id)+'|'+str(nb_occur))
+            print >> file_write, term+'|'+str(len(postinglist))+'||'+'||'.join(postinglist)
+        file_write.close()
+        print (str(sys.getsizeof(self.index))+"\n")
 
 
-    def getParams(self):
-        param=sys.argv
-        self.docsFolder=param[1]
-        self.indexFile=param[2]
+    def getParams(self,param):
+        self.docsFolder=param[1] #répertoire contient les fichiers docs (latimes)
+        self.indexFolder=param[2] #répertoire à écrire les fichiers indexes
+        self.mapFile = param[3] #fichier à écrire les correspondances des nouveaux docids
+        if not exists(self.indexFolder):
+            makedirs(self.indexFolder)
 
 
-    def createIndex(self):
-        self.getParams()
+    def createIndex(self,param):
+        self.getParams(param)
         self.getStopwords()
         docs_path = self.docsFolder
         files = [f for f in listdir(docs_path) if isfile(join(docs_path,f)) and splitext(f)[1]=='' and 'la' in splitext(f)[0]]
-        nb_files = 0
+        files = sorted(files)
+        nb_index_file = 0
+        doc_id =0 #doc id recompté par rapport à l'ordre d'apparaître
+        mapFile_write = open(self.mapFile,"w")
         for file in files:
-            # if nb_files<5:
-                file_name = splitext(file)[0]
-                nb_files += 1
-                print 'En cours '+file_name
-                f_read = open(docs_path+file,'r')
-                self.file_data = f_read
-                doc_dict=self.getDoc()
-                while doc_dict != {}:
-                    doc_content = doc_dict['content']
-                    doc_text = ''
-                    for p_content in doc_content.values():
-                        doc_text += p_content
-                    doc_id = int(doc_dict['doc_id'])
-                    terms = self.getTerms(doc_text)
-                    #construire indexe pour le document courant
-                    term_doc_dict = {}
-                    for position, term in enumerate(terms):
+            file_name = splitext(file)[0]
+            print 'En cours '+file_name
+            f_read = open(docs_path+file,'r')
+            self.file_data = f_read
+            doc_dict=self.getDoc()
+            while doc_dict != {}:
+                doc_content = doc_dict['content']
+                doc_text = ''
+                doc_id += 1
+                for p_content in doc_content.values():
+                    doc_text += p_content
+                docID = doc_dict['doc_id'].strip()
+                print >>mapFile_write, str(doc_id) + ":" + file_name + "-" + docID
+                terms = self.getTerms(doc_text)
+                #construire indexe pour le document courant
+                term_doc_dict = {}
+                for term in terms:
+                    try:
+                        term_doc_dict[term][doc_id] += 1
+                    except:
                         try:
-                            term_doc_dict[term][doc_id].append(position)
+                            term_doc_dict[term][doc_id] = 1
                         except:
-                            try:
-                                term_doc_dict[term][doc_id] = [position]
-                            except:
-                                term_doc_dict[term] = {}
-                                term_doc_dict[term][doc_id] = [position]
+                            term_doc_dict[term] = {}
+                            term_doc_dict[term][doc_id] = 1
 
-                        #ajouter indexe du document courant à l'indexe global
-                    for term_doc, posting_doc in term_doc_dict.iteritems():
-                            self.index[term_doc][file_name][doc_id] = posting_doc[doc_id]
-                    doc_dict = self.getDoc()
-                f_read.close()
-            # else:
-            #     break
-        self.writeFileIndex()
+                #ajouter indexe du document courant à l'indexe global
+                for term_doc, posting_doc in term_doc_dict.iteritems():
+                    self.index[term_doc][doc_id] = posting_doc[doc_id]
+                    if sys.getsizeof(self.index) > 1000000:
+                        print (str(sys.getsizeof(self.index))+"\n")
+                        nb_index_file += 1
+                        index_file = str(nb_index_file)+".txt"
+                        self.writeFileIndex(index_file)
+                        print nb_index_file
+                doc_dict = self.getDoc()
+            f_read.close()
+        if (len(self.index)>0):
+            print (str(sys.getsizeof(self.index))+"\n")
+            nb_index_file += 1
+            index_file = str(nb_index_file)+".txt"
+            self.writeFileIndex(index_file)
+            print nb_index_file
 
-    def verifyZipf(self):
-        f_read = open(self.indexFile,"r")
-        f_write = open("test1.txt","w")
-        list_fr = []
+def mergeIndex(files_folder, index_file):
+    files = [f for f in listdir(files_folder) if isfile(join(files_folder,f)) and splitext(f)[0].isdigit()]
+    files = sorted(files)
+    index = defaultdict(list)
+    for file in files:
+        f_read = open(files_folder+file,"r")
         for line in f_read:
-            line_arr = line.split("||")
-            freq = line_arr[1]
-            list_fr.append(freq)
-        list_fr.sort(reverse=True)
-        for freqe in list_fr:
-            f_write.writeln(freqe)
-        pyplot.plot(list_fr)
-        pyplot.savefig("test.png")
+            line = line.rstrip().split("||")
+            term = line[0].split("|")[0]
+            i = 1
+            while i<len(line):
+                doc = line[i].split("|")
+                index[term].append(doc)
+                i += 1
         f_read.close()
-        f_write.close()
+    index = sorted(index.items(), key = lambda item:item[0])
+    f_write = open(index_file,"w")
+    for term in index:
+        print >> f_write,term[0]+"|"+str(len(term[1]))+"||"+"||".join([doc[0]+"|"+doc[1] for doc in term[1]])
+    f_write.close()
+
 
 if __name__== "__main__":
-    index = IndexInverse()
-    index.createIndex()
-    # index.verifyZipf()
+    param=sys.argv
+    if len(param) == 4:
+        if param[1] == 'merge':
+            mergeIndex(param[2],param[3])
+        else:
+            index = IndexInverse()
+            index.createIndex(param)
+    else:
+        print "Erreur"
 
 
